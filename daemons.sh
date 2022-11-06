227db6b9ab585e5256c03cc0b1f0dd7e784a43ed
Arlo O'Keeffe <tiger@k5d.de>
2022-11-06 16:53:32 +0100

    add daemons.sh

---
+++ daemons.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -o nounset
+set -o errexit
+if [ "${1:-}" = "trace" ]; then
+    set -o xtrace
+fi
+if pgrep "VBoxHeadless" > /dev/null; then
+    echo ◼︎
+elif pgrep "limactl" > /dev/null; then
+    echo ◼︎
+elif pgrep "mutagen" > /dev/null; then
+    echo ◼︎
+elif pgrep -f "react-scripts" > /dev/null; then
+    echo ◼︎
+elif pgrep "java" > /dev/null; then
+    echo ◼︎
+else
+    echo ◻
+fi
